bd443d0989927b57b5ee3f37d3a043a01d762804
caohuilin <1057275848@qq.com>
2016-03-03 15:28:38 +0800

    rtg

--- src/jsx/Main.js
+++ src/jsx/Main.js
@@ -1,30 +1,26 @@
-const Main = React.createClass({
-    contextTypes: {
-        router: React.PropTypes.object.isRequired
-    },
-    componentWillMount(){
-        //给路径赋默认值
-        var params = this.props.params;
-        if (!params.day && !params.userNoteId && !params.depId) {
-            this.context.router.push("/day/" + moment().format('YYYY-MM-DD') + "/dep/-1");
-        }
-    },
-    //加载最终页面 两个组件Header和Content
-    render(){
-        var params = this.props.params;
-        if (!params){
-            return null;
-        }
-        if (!params.day && !params.userNoteId && !params.depId) {
-            return null;
-        }
-        return (
-            <div className="main">
-                <Header/>
-                <Content params={this.props.params}/>
-            </div>
-        )
-    }
+
+const Header = React.createClass({
+  render(){
+    return(
+      <header className='header'>
+        <span className='icon'></span>
+        <div className="logo">=CHL</div>
+        <span className='icon'></span>
+      </header>
+    );
+  }
 });
-
-
+const Content = React.createClass({
+  render(){
+    return(
+      <h1>Content</h1>
+    );
+  }
+});
+ReactDOM.render(
+  <div>
+    <Header />
+    <Content />
+  </div>,
+  document.getElementById('main')
+);
